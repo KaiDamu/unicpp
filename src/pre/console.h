280bf08991474aae29a98167eae2a971678271ef
KaiDamu <kaidamumail@gmail.com>
2022-12-02 16:20:20 +0100

    Added ConWriteRaw

--- src/pre/console.h
+++ src/pre/console.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define ConWrite(...) { printf(__VA_ARGS__); printf("\n"); }
+#define ConWriteRaw(...) { printf(__VA_ARGS__); }
 #define ConWriteInfo(...) { printf("[INFO] "); ConWrite(__VA_ARGS__); }
 #define ConWriteErr(...) { printf("[ERROR] "); ConWrite(__VA_ARGS__); }
 
