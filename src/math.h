c0be99121c2ca5849aeb76f37d36c19c3e5b31fe
KaiDamu <kaidamumail@gmail.com>
2022-12-03 22:03:09 +0100

    Added more math tools

--- src/math.h
+++ src/math.h
@@ -6,11 +6,29 @@ tpl1 dfa T1 Min(T1 a, T1 b) {
 tpl1 dfa T1 Max(T1 a, T1 b) {
 	ret ((a > b) ? a : b);
 }
-
+tpl1 dfa T1 Clamp(T1 val, T1 min, T1 max) {
+	ret Min<T1>(Max<T1>(val, min), max);
+}
+tpl1 dfa T1 Abs(T1 val) {
+	ret ((val < 0) ? -val : val);
+}
+tpl1 dfa T1 Lerp(T1 a, T1 b, T1 t) {
+	ret a + (b - a) * t;
+}
+tpl1 dfa T1 Sign(T1 val) {
+	ret ((val < 0) ? -1 : 1);
+}
+tpl1 dfa T1 Square(T1 val) {
+	ret val * val;
+}
 tpl1 dfa NT Swap(T1& a, T1& b) {
 	cx T1 tmp = a;
 	a = b;
 	b = tmp;
+}
+
+dfa S4 RoundF4ToS4(F4 val) {
+	ret S4((val < 0) ? (val - 0.5f) : (val + 0.5f));
 }
 
 tpl1 dfa SI VarintEncode(U1* out, T1 in) {
