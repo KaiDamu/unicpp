f22ab5948022b6d7e86886a6953237027bf2708c
KaiDamu <kaidamumail@gmail.com>
2023-02-11 17:27:03 +0100

    Console reference hotfix

--- src/pre/console.h
+++ src/pre/console.h
@@ -5,5 +5,6 @@ enum class ConCol : U1;
 dfa ER ConReq();
 dfa ER ConColSet(ConCol col);
 dfa ER ConColGet(ConCol& col);
+dfa ER ConWrite(cx CS* format, ...);
 dfa ER ConWriteErr(cx CS* format, ...);
 dfa ER ConPause();
